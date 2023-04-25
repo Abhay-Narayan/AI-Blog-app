4ab71edc97be7ca6a5c9f3b101192f9295c64a7c
AbhayETH <abhaynarayan16122002@gmail.com>
2023-04-25 16:52:53 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
 import Trending from "app/(home)/Trending"
-
+import Tech from "app/(home)/Tech"
+import Travel from "app/(home)/Travel"
+import Other from "app/(shared)/Other"
+import Subscribe from "./(shared)/Subscribe"
+import Sidebar from "app/(shared)/Sidebar"
 export default function Home() {
   return (
     <main className="px-10 leading-7">
       <Trending/>
       <div className="md:flex gap-10 mb-5" >
         <div className="basis-3/4">
-          {/* <Tech/> */}
-          {/* <Travel/> */}
-          {/* <Other/> */}
-          <div className="hidden md:block"></div>
+          <Tech/>
+          <Travel/>
+          <Other/>
+          <div className="hidden md:block"><Subscribe/></div>
         </div>
         <div className="basis-1/4">
-          {/* <Sidebar/> */}
+          <Sidebar/>
         </div>
       </div>
     </main>
